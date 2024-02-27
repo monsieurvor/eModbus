b808139a41f5bc988b59af7858b49c5b8e947c45
Yvar Dankers <80070435+monsieurvor@users.noreply.github.com>
2024-02-27 16:58:46 +0100

    a

--- src/Logging.h
+++ src/Logging.h
@@ -1,3 +1,14 @@
 #if defined(ESP32)
 #include <esp32-hal-log.h> // Use build flag `-D CORE_DEBUG_LEVEL` to set logging level
 #else
+#define log_v
+#define log_buf_v
+#define log_d
+#define log_buf_d
+#define log_i
+#define log_buf_i
+#define log_w
+#define log_buf_w
+#define log_e
+#define log_buf_e
+#endif
